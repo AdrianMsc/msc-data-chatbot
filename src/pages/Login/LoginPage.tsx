693739ebc484dec4cdc48f161c294bf21917b038
Adrian Lona <104098971+AdrianLonaFragoso@users.noreply.github.com>
2025-06-18 13:27:16 -0600

    feat: Login checkout

--- src/pages/Login/LoginPage.tsx
+++ src/pages/Login/LoginPage.tsx
@@ -40,39 +40,41 @@ const LoginPage = () => {
   };
 
   return (
-    <div className="max-w-md mx-auto mt-20 p-6 shadow-md rounded bg-white items-center flex flex-col justify-center">
-      <MscLogo width={150} />
-      <h1 className="text-2xl font-bold  mb-5">MSC Chatbot </h1>
-      <form onSubmit={handleSubmit} className="space-y-4 w-full">
-        <input
-          type="email"
-          placeholder="Correo"
-          className="w-full p-2 border rounded"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
+    <main className="h-screen w-screen  flex items-center justify-center">
+      <div className="max-w-md mx-auto p-6 shadow-md rounded bg-white items-center flex flex-col justify-center">
+        <MscLogo width={150} />
+        <h1 className="text-2xl font-bold  mb-5">Welcome</h1>
+        <form onSubmit={handleSubmit} className="space-y-4 w-full">
+          <input
+            type="email"
+            placeholder="Correo"
+            className="w-full p-2 border border-solid rounded"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
 
-        <input
-          type="password"
-          placeholder="Contraseña"
-          className="w-full p-2 border rounded"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
+          <input
+            type="password"
+            placeholder="Contraseña"
+            className="w-full p-2 border rounded"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
 
-        {error && <p className="text-red-500 text-sm">{error}</p>}
+          {error && <p className="text-red-500 text-sm">{error}</p>}
 
-        <button
-          type="submit"
-          disabled={loading}
-          className="w-full bg-blue-600 text-white py-2 rounded hover:bg-blue-700 transition"
-        >
-          {loading ? "Loading..." : "Login"}
-        </button>
-      </form>
-    </div>
+          <button
+            type="submit"
+            disabled={loading}
+            className="w-full bg-blue-600 text-white py-2 rounded hover:bg-blue-700 transition"
+          >
+            {loading ? "Loading..." : "Login"}
+          </button>
+        </form>
+      </div>
+    </main>
   );
 };
 
