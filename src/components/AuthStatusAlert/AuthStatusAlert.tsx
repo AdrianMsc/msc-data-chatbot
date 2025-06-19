630d46fb01e61aa52380b6d611e64c7845e01600
Adrian Lona <104098971+AdrianLonaFragoso@users.noreply.github.com>
2025-06-19 10:41:12 -0600

    feat: Auth design applied

--- src/components/AuthStatusAlert/AuthStatusAlert.tsx
+++ src/components/AuthStatusAlert/AuthStatusAlert.tsx
@@ -21,22 +21,29 @@ const AuthStatusAlert = () => {
   }, []);
 
   return isAuthenticated ? (
-    <div
-      className={`p-4 mb-4 text-sm text-green-800 rounded-lg bg-green-50 fixed z-50 ${
-        isVisible ? "flex" : "hidden"
-      }`}
-      role="alert"
-    >
-      <span className="font-medium">Authenticated!</span>
-    </div>
+    <>
+      <div className="bg-green-500 h-2 w-2 rounded-full fixed top-1 left-1 z-50"></div>
+      <div
+        className={`p-4 mb-4 text-sm text-green-800 rounded-lg bg-green-50 top-1 left-1 fixed z-50 ${
+          isVisible ? "flex" : "hidden"
+        }`}
+        role="alert"
+      >
+        <span className="font-medium">Authenticated!</span>
+      </div>
+    </>
   ) : (
-    <div
-      className="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50"
-      role="alert"
-    >
-      <span className="font-medium">¡No autenticado!</span> Por favor inicia
-      sesión para continuar.
-    </div>
+    <>
+      <div className="bg-red-500 h-2 w-2 rounded-full fixed top-1 left-1 z-50"></div>
+      <div
+        className={`p-4 mb-4 text-sm text-green-800 rounded-lg bg-green-50 top-1 left-1 fixed z-50 ${
+          isVisible ? "flex" : "hidden"
+        }`}
+        role="alert"
+      >
+        <span className="font-medium">¡Nope!</span>
+      </div>
+    </>
   );
 };
 
