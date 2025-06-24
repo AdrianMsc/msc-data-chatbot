1ede0d41ed3d603789d922aea73269a2887da1bf
Adrian Lona <104098971+AdrianLonaFragoso@users.noreply.github.com>
2025-06-24 08:45:45 -0600

    fix: imessage interface

--- src/components/core/chatMessage/ChatMessage.tsx
+++ src/components/core/chatMessage/ChatMessage.tsx
@@ -1,15 +1,15 @@
-import type { Message } from '../../../types/message';
+import type { IMessage } from "../../../types/message";
 
 interface ChatMessageProps {
-	message: Message;
+  message: IMessage;
 }
 
 const ChatMessage = ({ message }: ChatMessageProps) => {
-	return (
-		<div className="w-fit max-w-[70%] h-fit px-3 py-2 bg-primary-blue_light self-end rounded-lg chat-message">
-			{message.content}
-		</div>
-	);
+  return (
+    <div className="w-fit max-w-[70%] h-fit px-3 py-2 bg-primary-blue_light self-end rounded-lg chat-message">
+      {message.content}
+    </div>
+  );
 };
 
 export default ChatMessage;
